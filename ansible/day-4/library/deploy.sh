a9380ed86581c93b5ce3c3c471a2d99bd9168050
Pavel Kamyshev <kamyshevpavel@gmail.com>
2017-09-27 11:26:19 +0300

    add callback_plugin

--- ansible/day-4/library/deploy.sh
+++ ansible/day-4/library/deploy.sh
@@ -2,6 +2,6 @@
 
 source $1
 
-      curl --silent -T "${war}"  "${url}"
+      curl --silent -T "${war}" -u ${user}:${password} "${url}"
 
 printf '{"changed": true, "failed": false, "msg": "New application was deployed"}'
